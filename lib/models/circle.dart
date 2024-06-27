181ccbd8e3d485d8567d96f1abfabe17b7194231
st <stefanie@sdo.sh>
2024-06-27 10:47:23 +0200

    feature: #5 create circle model

---
+++ lib/models/circle.dart
@@ -0,0 +1,31 @@
+import 'dart:math';
+
+import 'shape.dart';
+
+/// The [Circle] is a specific kind of [Shape].
+///
+/// It needs a [radius] for the calculations to work.
+class Circle extends Shape {
+  /// The [radius] is expected to be a double to avoid casting.
+  double radius;
+
+  Circle({
+    required int id,
+    required String name,
+    required this.radius,
+  }) : super(
+    name: name,
+  );
+
+  @override
+  double calculateArea() {
+    var area = pi * radius * radius;
+    return double.parse(area.toStringAsFixed(2));
+  }
+
+  @override
+  double calculateCircumference() {
+    var circumference = 2 * pi * radius;
+    return double.parse(circumference.toStringAsFixed(2));
+  }
+}
